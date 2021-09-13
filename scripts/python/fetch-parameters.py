d3636c777912226b71341fd1c8471862ab1082f7
Arif Pehlivan <pehlivan.arif@dpa.com>
2021-09-13 21:59:15 +0200

    Create json file from parameters

--- scripts/python/fetch-parameters.py
+++ scripts/python/fetch-parameters.py
@@ -1,7 +1,6 @@
 import os
 import boto3
-
-ssm_client = boto3.client('ssm')
+from botocore.config import Config
 
 def getEnvironmentParameters():
     mongodbUriParameter = '/config/'+os.environ['SERVICE_NAME']+'_'+os.environ['APPLICATION_ENV']+'/mongodbUri'
@@ -11,14 +10,20 @@ def getEnvironmentParameters():
     mongodbUri=""
     userAdminRootUrl=""
     userExpiredQueueUrl=""
+    f= open("env.json","w+")
+    f.write("{ \n")
     for p in result["Parameters"]:
         if p["Name"] == mongodbUriParameter:
             mongodbUri = p["Value"]
+            f.write("\"MONGO_URL\": \"%s\", \n"%(mongodbUri))
         if p["Name"] == userAdminRootUrlParameter:
             userAdminRootUrl = p["Value"]
+            f.write("\"USER_ADMIN_ROOT_URL\": \"%s\", \n"%(userAdminRootUrl))
         if p["Name"] == userExpiredQueueUrlParameter:
             userExpiredQueueUrl = p["Value"]
-
+            f.write("\"USER_EXPIRED_QUEUE_URL\": \"%s\" \n"%(userExpiredQueueUrl))
+    f.write("}")
+    f.close()
     keyValue={"mongodbUri": mongodbUri, "userAdminRootUrl":userAdminRootUrl, "userExpiredQueueUrl": userExpiredQueueUrl}
     print(keyValue)
     return keyValue
@@ -29,6 +34,8 @@ def get_parameters(parameter_names):
     :return: Return parameter details if exist else None
     """
     try:
+        ssm_config = Config(region_name = 'eu-central-1')
+        ssm_client = boto3.client('ssm',config=ssm_config)
         result = ssm_client.get_parameters(Names=parameter_names)
     except e:
         print('ERROR: Get parameters from AWS Parameter: %s' % e)
@@ -37,7 +44,9 @@ def get_parameters(parameter_names):
 
 def main():
     os.environ['SERVICE_NAME'] = "audio-hub-user-administration"
-    os.environ['APPLICATION_ENV'] = "devel"
+    os.environ['APPLICATION_ENV'] = "APPLICATION_ENV"
+    os.environ['AWS_ACCESS_KEY_ID'] = "AWS_ACCESS_KEY_ID"
+    os.environ['AWS_SECRET_ACCESS_KEY'] = "AWS_SECRET_ACCESS_KEY/keyID"
     getEnvironmentParameters()
 
 
