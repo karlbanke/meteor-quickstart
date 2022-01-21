e5b973571b0a099ce36deeb346658a3c85af4c56
Arif Pehlivan <pehlivan.arif@dpa.com>
2022-01-21 10:27:07 +0100

    Install chrome drive for selenium tests

--- scripts/build-meteor.sh
+++ scripts/build-meteor.sh
@@ -17,6 +17,22 @@ chown -R node:node /opt/nodejs /opt/meteor
 # Enabling implicit installation of fibers
 sudo su - node -s /bin/bash -c 'npm install --global node-gyp'
 
+
+# Download and Install chromedriver
+wget -N https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
+
+
+# Install chrome browser
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+apt-get -y update
+apt-get -y install google-chrome-stable
+
 # 
 env 
 
