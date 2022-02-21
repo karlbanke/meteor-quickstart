0d654c2267b6ea6d00e1b9de486d76580c6b61c8
Arif Pehlivan <pehlivan.arif@dpa.com>
2022-02-21 17:32:42 +0100

    Run scripts with ubuntu 20.04 LTS (focal) version

---
+++ scripts/ubuntu-focal-install-deps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+printf "\n[-] Installing base OS dependencies...\n\n"
+
+apt-get update -y
+apt-get install -y --no-install-recommends sudo curl libarchive-tools ca-certificates bzip2 gcc g++ make build-essential python git wget gnupg
+apt-get install -y xauth libgtk2.0-0 libgconf-2-4 libasound2 libxtst6 libxss1 libnss3 xvfb
+
+dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
+
+wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch"
+wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch.asc"
+
+export GNUPGHOME="$(mktemp -d)"
+
+echo "buster-install-deps.sh: Add keyserver"
+#echo "disable-ipv6" >> "$GNUPGHOME"/dirmngr.conf;
+gpg -v --keyserver keyserver.ubuntu.com --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
+echo "buster-install-deps.sh: Verify deps"
+gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu
+
+rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc
+
+chmod +x /usr/local/bin/gosu
+
+gosu nobody true
+
+apt-get purge -y --auto-remove wget
+
+apt-get -y autoremove && apt-get clean
